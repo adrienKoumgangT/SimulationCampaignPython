fcebbc4e2ab00a1f546f9b438058e350a5c95999
Adrien09 <64235641+adrienKoumgangT@users.noreply.github.com>
2021-12-26 00:49:19 +0100

    update simulator

--- src/simulator/simulator.py
+++ src/simulator/simulator.py
@@ -66,15 +66,16 @@ def get_simulator_info():
 
 
 def get_info_nb_values(name_var: int):
-    if name_var in {1, 2, 3, 4, 5, 6, 7}:
-        # nb = 41 [100 -> 500]; shift = 10
-        return 50
+    if name_var in {0, 1, 2, 3, 4, 5, 6, 7}:
+        return 100
     else:
-        # nb = 1 [100]; shift = 0
-        return 0
+        return 1
 
 
 def get_info_values_var(name_var: int):
+    if name_var == 0:
+        # var = "nb nodes; nb = 1 []; shift = 1
+        return 100, 150, 10
     if name_var == 1:
         # var = "nb infected"; nb = 10 [1 -> 10]; shift = 1
         return 1, 10, 1
@@ -95,7 +96,7 @@ def get_info_values_var(name_var: int):
         return 150, 300, 10
     elif name_var == 7:
         # var = "immune period"; nb = 11 [200 -> 400]; shift = 20
-        return 200, 400, 10
+        return 200, 400, 20
     else:
         # nb = 1 [1]; shift = 1
         return 1, 1, 1
@@ -113,30 +114,26 @@ def run_simulator(variable_interest: str, variable_value_begin: int, variable_va
                     '-stop_all_sane=1', '-gui=0', '-printout=2']
         for other_arg in list_other_interest_var:
             list_arg.append(other_arg)
-        simulation_index = 0
+        simulation_index = 1
         while simulation_index <= nb_simulation_to_do:
             print(f"\t\t --> simulation n. {simulation_index}")
             res = subprocess.run(list_arg, capture_output=True)
-            with open(path_directory + 'res_value_variable_' + str(var_value) + '_simulation_n_' +
-                      str(simulation_index) + '.txt', 'w') as file_result:
+            # TODO: change name file to result
+            with open(f"{path_directory}res_{variable_interest[1:]}_{var_value}_simulation_n_{simulation_index}.txt",
+                      'w') as file_result:
                 file_result.write(res.stdout.decode('utf-8'))
             simulation_index += 1
 
     # Pour empêcher des cycle infini
     if variable_incremented_value <= 0:
-        variable_incremented_value = 1
-    if variable_value_begin > variable_value_end:
-        val_inter = variable_value_end
-        variable_value_end = variable_value_begin
-        variable_value_begin = val_inter
-
+        return
     if number_of_simulation <= 0:
-        number_of_simulation = 1
+        return
 
     # début de la simulation
 
     var_value = variable_value_begin
-    while var_value <= variable_value_end:
+    while variable_value_begin <= var_value <= variable_value_end:
         print(f"Simulation for {variable_interest} = {var_value}")
         do_simulation(value_of_variable=var_value, nb_simulation_to_do=number_of_simulation)
         var_value += variable_incremented_value
@@ -166,8 +163,8 @@ if __name__ == '__main__':
             if option[0] == "-name_var":
                 name_variable = int(option[1])
                 var_interest = get_name_variable(name_var=name_variable)
-            elif option[0] == "-nb_simulation":
-                nb_nodes_incremented = int(option[1])
+            elif option[0] == "-number_of_simulation":
+                nb_simulation = int(option[1])
             elif option[0] == "-variable_value_begin":
                 var_value_begin = int(option[1])
             elif option[0] == "-variable_value_end":
@@ -185,7 +182,7 @@ if __name__ == '__main__':
     if name_variable in {0, 1, 2, 3, 4, 5, 6, 7}:
         run_simulator(variable_interest=var_interest, variable_value_begin=var_value_begin,
                       variable_value_end=var_value_end, variable_incremented_value=var_incremented_value,
-                      number_of_simulation=1, list_other_interest_var=list_other_parameter_simulator,
+                      number_of_simulation=nb_simulation, list_other_interest_var=list_other_parameter_simulator,
                       path_directory=pathname)
 
     else:
